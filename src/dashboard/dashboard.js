0111deb80435e1ae145b5c2a0575c69c251c4bb3
Ralph <ralph13@me.com>
2019-02-24 15:26:18 -0500

    Added the tempAppID, tempAppIDChange and dashHistory props.

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -9,17 +9,21 @@ class Dashboard extends React.Component {
     render() {
         // State passed down from "App" component
         const appID = this.props.appID;
+        const tempAppID = this.props.tempAppID;
         const appIDChange = this.props.appIDChange;
+        const tempAppIDChange = this.props.tempAppIDChange
         const keywordChange = this.props.keywordChange;
         const keywords = this.props.keywords;
-        const history = this.props.history;
+        const dashHistory = this.props.history;
 
         return(
             <NavBar bg="dark">
                 <ChangeAppID 
                     appID={ appID }
+                    tempAppID={ tempAppID }
+                    tempAppIDChange={ tempAppIDChange }
                     appIDChange={ appIDChange }
-                    history={ history }>
+                    dashHistory={ dashHistory }>
                 </ChangeAppID>
                 <KeywordSearch keywords={ keywords } keywordChange={ keywordChange }/>
             </NavBar>
