d317d2f1c553977cb63e2029e03ae256fb06972d
Ralph <ralph13@me.com>
2019-02-24 15:15:19 -0500

    Added a VerifyAppID function which runs a test request to eBay's api to validate the supplied appID.

--- utils/verify_app_id.js
+++ utils/verify_app_id.js
@@ -1,12 +1,50 @@
-function VerifyAppID (appID, history) {
-    if(!appID) {
-        alert('The field is blank.  Please enter the App ID.')
-    } else if (history.location.pathname === "/dashboard") {
-        console.log('Success.  App_ID is: ' + appID)
-        alert('App ID was changed to "' + appID + '".')
-    } else {
-        history.push('/dashboard')
-    }
+const https = require('https');
+
+function VerifyAppID (appID) {
+
+    var baseUrl = 'https://svcs.ebay.com/services/search/FindingService/v1?'
+        baseUrl += 'OPERATION-NAME=findCompletedItems&'
+        baseUrl += 'SERVICE-VERSION=1.13.0&'
+        baseUrl += 'GLOBAL-ID=EBAY-US&'
+        baseUrl += 'SECURITY-APPNAME=' + appID + '&'
+        baseUrl += 'RESPONSE-DATA-FORMAT=JSON&'
+        baseUrl += 'REST-PAYLOAD&'
+        baseUrl += 'keywords=harry%20potter%20phoenix&'
+        baseUrl += 'itemFilter(0).name=Condition&'
+        baseUrl += 'itemFilter(0).value=3000'
+
+    let promise = new Promise((resolve, reject) => {
+        let authenticated = false;
+
+        https.get(baseUrl, (response) => {
+            let data = '';
+
+            response.on('data', (chunk) => {
+                data += chunk;
+            });
+
+            response.on('end', () => {
+                const jsonData = JSON.parse(data);
+                if (response.statusCode == 200 && jsonData.findCompletedItemsResponse) {
+                    authenticated = true;
+                    resolve(authenticated)
+                } else if (response.statusCode === 500) {
+                    try {
+                        var auth_message = jsonData.errorMessage[0].error[0].message[0]
+                        console.log(auth_message)
+                        reject(authenticated);
+                    } catch (error) {
+                        console.log('An unknown error has occurred.  Please retry authentication.')
+                        reject(authenticated);
+                    }
+                }
+            })
+
+        })
+
+    })
+
+    return promise;
 }
 
 export default VerifyAppID;
