e17c3382c857b761b68c74144651e2873ff4df65
Ralph <ralph13@me.com>
2019-02-28 18:58:51 -0500

    Added container to view avg sales price

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import NavBar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
 import ChangeAppID from './change_app_id'
 import KeywordSearch from './keyword_search'
 
@@ -10,23 +11,38 @@ class Dashboard extends React.Component {
         // State passed down from "App" component
         const appID = this.props.appID;
         const tempAppID = this.props.tempAppID;
+        const keywords = this.props.keywords;
+        const avgSalePrice = this.props.avgSalePrice;
         const appIDChange = this.props.appIDChange;
         const tempAppIDChange = this.props.tempAppIDChange
         const keywordChange = this.props.keywordChange;
-        const keywords = this.props.keywords;
+        const handleAvgSalesChange = this.props.handleAvgSalesChange;
         const dashHistory = this.props.history;
 
         return(
-            <NavBar bg="dark">
-                <ChangeAppID 
-                    appID={ appID }
-                    tempAppID={ tempAppID }
-                    tempAppIDChange={ tempAppIDChange }
-                    appIDChange={ appIDChange }
-                    dashHistory={ dashHistory }>
-                </ChangeAppID>
-                <KeywordSearch keywords={ keywords } keywordChange={ keywordChange }/>
-            </NavBar>
+            <div>
+                <NavBar bg="dark">
+                    <ChangeAppID 
+                        appID={ appID }
+                        tempAppID={ tempAppID }
+                        tempAppIDChange={ tempAppIDChange }
+                        appIDChange={ appIDChange }
+                        dashHistory={ dashHistory }>
+                    </ChangeAppID>
+                    <KeywordSearch 
+                        appID={ appID } 
+                        keywords={ keywords } 
+                        keywordChange={ keywordChange } 
+                        handleAvgSalesChange={ handleAvgSalesChange }
+                    />
+                </NavBar>
+                <Container>
+                    <div>
+                        { avgSalePrice }
+                    </div>
+                </Container>
+            </div>
+            
         );
     }
 }
