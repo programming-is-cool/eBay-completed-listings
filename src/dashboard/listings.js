d84f91d5f09e84b3ad550f04289fd0f9d822e39b
Ralph <ralph13@me.com>
2019-03-07 18:29:01 -0500

    changed from grid to listing layout

--- src/dashboard/listings.js
+++ src/dashboard/listings.js
@@ -1,41 +1,37 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card'
-import CardColumns from 'react-bootstrap/CardColumns'
-import CardDeck from 'react-bootstrap/CardDeck'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 require('../../assets/css/bootstrap.min.css')
 
 function Listings(props) {
-    const rows = [...Array( Math.ceil(props.listings.length / 3) )];
-    const productRows = rows.map( (row, idx) => props.listings.slice(idx * 3, idx * 3 + 3) );
-    const content = productRows.map((row, idx) => (
-        <Row key={ idx + Math.random() }>
-            <CardColumns>
-                {row.map((product, i) => 
-                    <Card key={ i + Math.random() } border="secondary" style={{ width: '18rem' }} >
-                        <Card.Img variant="top" src={ product.image } />
-                        <Card.Body>
-                            <Card.Title>
-                                { product.title }
-                            </Card.Title>
-                            <Card.Text>
-                                Condition:
-                                { ' ' + product.condition }
-                            </Card.Text>
-                            <Card.Text>
-                                { product.sellingStatus === "EndedWithSales" ? "Sold" : "Unsold" }
-                            </Card.Text>
-                            <Card.Text className={ product.sellingStatus === "EndedWithSales" ? "text-success" : "text-danger" }>
-                                Price:
-                                { ' $' + product.price }
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                )}
-            </CardColumns>
-        </Row>
-    ))
+    const content = props.listings.map((listing) =>
+        <Card className={ "listings-elems" } >
+            <Row className={ "no-gutters" }>
+                <Col md={ 3 }>
+                    <Card.Img variant="top" src={ listing.image } className={ "img-thumbnail" }/>
+                </Col>
+                <Col md={ 9 }>
+                    <Card.Body>
+                        <Card.Title>
+                            { listing.title }
+                        </Card.Title>
+                        <Card.Text>
+                            Condition:
+                            { ' ' + listing.condition }
+                        </Card.Text>
+                        <Card.Text>
+                            { listing.sellingStatus === "EndedWithSales" ? "Sold" : "Unsold" }
+                        </Card.Text>
+                        <Card.Text className={ listing.sellingStatus === "EndedWithSales" ? "text-success" : "text-danger" }>
+                            Price:
+                            { ' $' + listing.price }
+                        </Card.Text>
+                    </Card.Body>
+                </Col>
+            </Row>
+        </Card>
+    );
     return (
         <div>
             { content }
